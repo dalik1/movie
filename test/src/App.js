b692b7e361fd15454b2a9c5aa72c5f3a56d9fcb6
Mateus S. Oliveira <92745061+dalik1@users.noreply.github.com>
2024-02-20 15:43:12 -0300

    20/02

---
+++ test/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './global.css';
+
+function App() {
+  return (
+    
+  );
+}
+
+export default App;
---
+++ test/src/page/home/index.js
@@ -0,0 +1,7 @@
+export function Container(){
+    return(
+        <div class="container">
+
+        </div>
+    )
+}
