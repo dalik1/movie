790349e40945deacf8a531d421a5f31360744773
Mateus da Oliveira <mateusmateu17@gmail.com>
2024-03-13 14:35:55 -0300

    13/03

--- cineflix/src/page/carousel/index.js
+++ cineflix/src/page/carousel/index.js
@@ -2,10 +2,9 @@
 
 export function Carousel(){
     return(
-        <div class="carousel slide" data-bs-ride="carousel" id="ads" data-bs-touch="true">
-            <div>
+        <div class="carousel slide"  data-bs-interval="false" data-bs-ride="carousel" id="ads" data-bs-touch="true">
                 <div class="carousel-inner">
-                    <CarouselInner></CarouselInner>
+                    <CarouselInnerActive></CarouselInnerActive>
                     <CarouselInner></CarouselInner>
                     <CarouselInner></CarouselInner>
                 </div>
@@ -17,19 +16,30 @@ export function Carousel(){
                     <span class="carousel-control-next-icon bg-dark opacity-75 rounded-circle">
                     </span>
                 </button>
-            </div>
         </div>
     )
 }
 
 function CarouselInner(){
     return(
-        <div class="carousel-item active" data-bs-interval="none" >
+        <div class="carousel-item">
             <img class="films col-3 mid-cresce" src="https://www.themoviedb.org/t/p/w1280/miif4N3YWL5mGGlXA2YrHPr2iwz.jpg" alt="Duna"/>
             <img class="films col-3 mid-cresce" src="https://www.themoviedb.org/t/p/w1280/miif4N3YWL5mGGlXA2YrHPr2iwz.jpg" alt="Duna"/>
             <img class="films col-3 mid-sai" src="https://www.themoviedb.org/t/p/w1280/miif4N3YWL5mGGlXA2YrHPr2iwz.jpg" alt="Duna"/>
             <img class="films col-3 mid-sai" src="https://www.themoviedb.org/t/p/w1280/miif4N3YWL5mGGlXA2YrHPr2iwz.jpg" alt="Duna"/>
-            <iframe class="video" src="https://www.youtube.com/embed/ncwsW3qxQlo" title="Duna: Parte 2 | Trailer Oficial 3" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <iframe class="video" width="1209" height="679" src="https://www.youtube.com/embed/ILAwV65XuGA" title="&quot;Oppenheimer&quot; - Trailer Final Legendado (Universal Pictures Portugal)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+    )
+}
+
+function CarouselInnerActive(){
+    return(
+        <div class="carousel-item active">
+            <img class="films col-3 mid-cresce" src="https://www.themoviedb.org/t/p/w1280/miif4N3YWL5mGGlXA2YrHPr2iwz.jpg" alt="Duna"/>
+            <img class="films col-3 mid-cresce" src="https://www.themoviedb.org/t/p/w1280/miif4N3YWL5mGGlXA2YrHPr2iwz.jpg" alt="Duna"/>
+            <img class="films col-3 mid-sai" src="https://www.themoviedb.org/t/p/w1280/miif4N3YWL5mGGlXA2YrHPr2iwz.jpg" alt="Duna"/>
+            <img class="films col-3 mid-sai" src="https://www.themoviedb.org/t/p/w1280/miif4N3YWL5mGGlXA2YrHPr2iwz.jpg" alt="Duna"/>
+            <iframe class="video" width="1209" height="679" src="https://www.youtube.com/embed/ILAwV65XuGA" title="&quot;Oppenheimer&quot; - Trailer Final Legendado (Universal Pictures Portugal)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
     )
 }
