9680b085c982af6a94e1bbbcdc16a7b9ff5ac54b
Mateus da Oliveira <mateusmateu17@gmail.com>
2024-04-30 14:33:15 -0300

    30/04

--- theMovie/src/page/header/index.js
+++ theMovie/src/page/header/index.js
@@ -7,9 +7,9 @@ export function Cabecalio() {
                 <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
             </svg>
             <form>
-                <input type="text" id="pesquisa"  class="form-control pesquisa-control search-bar"></input>
-                <label for="pesquisa" class="form-label pesquisa-label" id="search-icon" onclick="toggleSearch()">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-search pesquisa-icon" viewBox="0 0 16 16">
+                <input type="text" id="pesquisa"  class="form-control pesquisa-control"></input>
+                <label for="pesquisa" class="form-label pesquisa-label" id="search-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-search pesquisa-icon" viewBox="0 0 16 16" onclick="abrirPesquisa()">
                             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
                     </svg>
                 </label>
@@ -17,11 +17,3 @@ export function Cabecalio() {
         </div>
     )
 }
-export function toggleSearch() {
-    var searchBar = document.getElementById("search-bar");
-    if (searchBar.style.display === "none") {
-      searchBar.style.display = "block";
-    } else {
-      searchBar.style.display = "none";
-    }
-  }
